959326dad1d3b16317d3568b9df1aba3d5d6bee3
Daniel <dancomes92@gmail.com>
2021-05-27 14:40:32 -0300

    modelos modificados

--- models/Usuario.js
+++ models/Usuario.js
@@ -8,8 +8,9 @@ const usuarioSchema = new mongoose.Schema({
         foto: {type: String, required: true},
         provincia: {type: String, required: true},
         administrador: {type: boolean, default: false},
-        carrito:  [String],
-        favoritos: [String]	
+        google: {type: boolean, default: false},
+        carrito: [{type: mongoose.Types.ObjectId, ref: 'product'}],
+        favoritos: [{type: mongoose.Types.ObjectId, ref: 'product'}]
 })
 
 const Usuario = mongoose.model('user', usuarioSchema)
