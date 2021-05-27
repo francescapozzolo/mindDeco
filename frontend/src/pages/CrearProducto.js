baec6d393819760fc678fa3cf5090bf00b84ab03
Francesca Pozzolo <pozzolofrance@gmail.com>
2021-05-27 15:30:36 -0300

    Comencé con formulario para cargar productos

--- frontend/src/pages/CrearProducto.js
+++ frontend/src/pages/CrearProducto.js
@@ -1,10 +1,36 @@
 
 const CrearProducto = () => {
+
+    const categorias = [
+        {categoria: 'living', subcategorias: ['muebles', 'textil', 'decoracion']}, 
+        {categoria:'cocina & comedor', subcategorias: ['muebles', 'textil', 'decoracion', 'vajilla']}, 
+        {categoria:'dormitorio', subcategorias: ['muebles', 'textil', 'decoracion']}, 
+        {categoria:'baños', subcategorias: ['muebles', 'textil', 'decoracion']}, 
+        {categoria:'jardin', subcategorias: ['muebles', 'textil', 'decoracion']}
+    ]
+
     return (
-        <div>
-            <select></select>
-            <select></select>
-        </div>
+        <form>
+            <select name='categorias' onChange={}>
+                {categorias.map(categoria => {
+                    return(
+                        <>
+                            <option key={categoria.categoria} value={categoria.categoria}>{categoria.categoria}</option>
+                        </>
+                    )
+                })}
+            </select>
+            <select name='subcategoria' onChange={}>
+                {categorias.subcategorias.map(subcategoria => {
+                    return(
+                        <>
+                            <option key={subcategoria} value={subcategoria}>{subcategoria}</option>
+                        </>
+                    )
+                })}
+
+            </select>
+        </form>
     )
 }
 
