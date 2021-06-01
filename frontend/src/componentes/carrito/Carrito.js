2048109a31816e639dfc287e67ab475f0d2dc458
Kevin Baez <soykevinkalen@gmail.com>
2021-05-31 22:01:52 -0300

    falta lo ultimo del tablero

--- frontend/src/componentes/carrito/Carrito.js
+++ frontend/src/componentes/carrito/Carrito.js
@@ -3,11 +3,11 @@ import {connect} from "react-redux"
 import carritoActions from "../../redux/actions/carritoActions"
 import authActions from "../../redux/actions/authActions"
 import Producto from './Producto';
-
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import ForumIcon from '@material-ui/icons/Forum';
 const Carrito = (props) => {
-    const [num, setNum] = useState(0)
     const [carrito, setCarrito] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [open, setOpen] = useState(false)
     let precioTotal = 0
     let articulosTotales = 0
     useEffect(() => {
@@ -45,14 +45,45 @@ const Carrito = (props) => {
                         
                 })
                 }
-                {/* <Producto /> */}
             </div>
             <div className='BTableroCarrito'>
                 <div className='BTableroContenido'>
-                    <h3>{articulosTotales+' artículos'}</h3>
-                    <h3>{precioTotal}</h3>
+                    <div className='BTableroFilas'>
+                        <h3>Envío</h3>
+                        <h3>Por calcular</h3>
+                    </div>
+                    <div className='BTableroFilas BMargginBoton'>
+                        <h3>{articulosTotales+' artículos'}</h3>
+                        <h3>{precioTotal + ' ARS'}</h3>
+                    </div>
+                    <hr />
+                    <h3 className='BMarginTop BCodigoPromocional' onClick={()=> setOpen(!open)}>¿Tienes un código promocional?</h3>
+                    {
+                        open && <div className='BTableroFilas BFilaInput'>
+                                    <input type="text" placeholder='Código promocional' className='BInputTablero'/>
+                                    <button className='BButon'>AÑADIR</button>
+                                </div>
+                    }
+                    
+                    <div className='BTableroFilas BTableroTotal'>
+                        <h3>Total</h3>
+                        <h3 className='BBold'>{precioTotal + ' ARS'}</h3>
+                    </div>
                 </div>
                 <button className='BButon'>COMPRAR</button>
+                <div>
+                    <div>
+                     <h3><LocalShippingIcon /> Política de Envío</h3>
+                     <h3>El envío está a cargo del comprador</h3>
+                    </div>
+                    <div>
+                        <h3>Cambios y Devoluciones</h3>
+                    </div>
+                    <div>
+                        <h3><ForumIcon /> Dudas? Te ayudamos?</h3>
+                        <h3>Comunicate con nosotros por whatsapp!</h3>
+                    </div>
+                </div>
             </div>          
         </div>
     )
